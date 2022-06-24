8a5e8743295d8fb5bc7547cd78a9a67ea5305763
Eke Timothy <eketimothy84@gmail.com>
2022-06-24 23:13:47 +0100

    Initial commit

---
+++ P_Info.js
@@ -0,0 +1,5 @@
+const name= "EKE TIMOTHY CHIEMEZIEM";
+const height = "6.4";
+const country = "Nigeria";
+
+alert(`My Name is ${name} \nI am ${height}ft tall. \nA Citizen of ${country} \nbut just for a little while`)
